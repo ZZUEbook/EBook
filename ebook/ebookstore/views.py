2f4455bd8240b98e606f64a24653676778cd8012
zzuwenjie <wen20121724@163.com>
2018-09-05 00:33:19 -0700

    add html files

--- ebook/ebookstore/views.py
+++ ebook/ebookstore/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("test!")
+    return render(request, template_name="ebookstore/Login.html")
