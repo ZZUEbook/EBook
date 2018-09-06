c499adf175f652d09444a6bd756e1c338d2b47ff
1dooooo <352387963@qq.com>
2018-09-06 14:01:02 +0800

    ido modify html url

--- ebook/ebookstore/urls.py
+++ ebook/ebookstore/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('index', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('admin', views.administrator, name='administrator'),
+    path('admin_login', views.administrator_Login, name='administrator_Login'),
 ]
--- ebook/ebookstore/views.py
+++ ebook/ebookstore/views.py
@@ -5,4 +5,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(request, template_name="ebookstore/index.html")
+
+def login(request):
     return render(request, template_name="ebookstore/Login.html")
+
+def register(request):
+    return render(request,template_name="ebookstore/Register.html")
+
+def administrator(request):
+    return render(request, template_name="ebookstore/Administrator.html")
+
+def administrator_Login(request):
+    return render(request, template_name="ebookstore/Administrator-Login.html")
