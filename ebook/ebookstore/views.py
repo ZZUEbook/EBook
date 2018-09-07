7d17694f48f06c53c07e8b09c77d5dba18ff15ad
zzuwenjie <wen20121724@163.com>
2018-09-07 08:53:48 +0800

    ajax

--- ebook/ebookstore/views.py
+++ ebook/ebookstore/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render
+from ebookstore.models import User
 
 # Create your views here.
 def index(request):
     return render(request, template_name="ebookstore/index.html")
 
 def login(request):
+    if request.method == "POST":
+        user = User.objects.filter(user_name=request.POST['name'])
+        if user.user_password == request.POST['password']:
+            1 == 1
+        print(request.POST)
     return render(request, template_name="ebookstore/Login.html")
 
 def register(request):
